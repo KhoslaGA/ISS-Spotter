5ad7fc90a6f57bed48a4eff0514d0ade66402f43
KhoslaGA <90459399+KhoslaGA@users.noreply.github.com>
2023-03-22 00:57:31 -0400

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,16 @@
+// The main function for ISS Spotter
+// jshint esversion : 6
+
+const nextISSTimesForMyLocation = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+    .then(nextTimes => {
+        for (let nextTime of nextTimes) {
+            let dateTime = new Date(0);
+            dateTime.setUTCSeconds(nextTime.risetime);
+            console.log(`Next pass at ${dateTime} for ${nextTime.duration} seconds!`);
+        }
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
